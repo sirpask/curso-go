09ffc3307328540ab5cf4278f28f9c1418697d68
sirpask <el_pask@hotmail.com>
2017-10-17 09:51:24 +0200

    nuevo archivo: variables.go

---
+++ variables.go
@@ -0,0 +1,23 @@
+package main 
+
+import ("fmt"
+        "time"
+        )
+
+func main (){
+	var suma int = 8 + 9
+	var resta int = 6 - 4
+	var nombre string = "david"
+
+	var apellidos string = "David WEB"
+	var apellidos = "Pascual"
+
+	pais:= "España"
+
+	fmt.Println("Hola mundo desde Go con " + nombre + apellidos + pais)
+	fmt.Println(suma)
+	fmt.Println(resta
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("Hola MUndo desde GO con Pask")
+} 
